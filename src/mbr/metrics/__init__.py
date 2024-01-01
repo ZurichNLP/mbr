871c225df24c8b9ef1ebbc535b58fb096cc247b0
Jannis Vamvas <vamvas@cl.uzh.ch>
2024-01-01 13:02:28 +0100

    Extend load_metric_runner function for fastchrf

--- src/mbr/metrics/__init__.py
+++ src/mbr/metrics/__init__.py
@@ -3,8 +3,11 @@ from mbr.metrics.base import metric_is_source_based, MetricRunner
 
 
 def load_metric_runner(mbr_config: MBRConfig, tokenizer=None) -> MetricRunner:
-    if mbr_config.metric == "fastchrf":
+    if mbr_config.metric in ["fastchrf", "fastchrf-aggregate"]:
         from mbr.metrics.fastchrf import FastChrfMetricRunner
         return FastChrfMetricRunner(mbr_config, tokenizer)
+    elif mbr_config.metric == "fastchrf-pairwise":
+        from mbr.metrics.fastchrf import FastChrfMetricRunner
+        return FastChrfMetricRunner(mbr_config, tokenizer, compute_pairwise_average=True)
     else:
         return MetricRunner(mbr_config, tokenizer)
